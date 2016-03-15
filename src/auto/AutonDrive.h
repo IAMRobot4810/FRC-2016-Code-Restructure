716464d7659202951bc89eed105728d8a138c1e3
Alex Phan <alexp7361@gmail.com>
2016-03-15 18:35:33 -0400

    AutonDrive update
    Code errors fixe
    
    Signed-off-by: Alex Phan <alexp7361@gmail.com>

--- src/auto/AutonDrive.h
+++ src/auto/AutonDrive.h
@@ -1,11 +1,14 @@
 #ifndef AUTO_AUTONDRIVE_H
 #define AUTO_AUTONDRIVE_H
 
+#include "WPILib.h"
+#include "systems/DriveSystem.h"
+
 class AutonDrive : public DriveSystem{
 private:
 public:
 	void TimedDrive(float time, float lPower, float rPower);
-	void EncoDrive(int lEncoderVal, int rEncoderVal)
+	void EncoDrive(int lEncoderVal, int rEncoderVal);
 };
 
 
