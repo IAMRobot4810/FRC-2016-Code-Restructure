3768a593759c7ff601c003a85d168d2efca57090
Alex Phan <alexp7361@gmail.com>
2016-03-22 17:53:32 -0400

    Added back DriveSystem, copied from master
    22-03-16
    
    Signed-off-by: Alex Phan <alexp7361@gmail.com>

---
+++ src/systems/DriveSystem.cpp
@@ -0,0 +1,68 @@
+/*
+ * DriveSystem.cpp
+ *
+ *  Created on: Feb 7, 2016
+ *      Author: 1750800404
+ */
+
+#include "DriveSystem.h"
+
+DriveSystem::DriveSystem(){
+	flTal = new CANTalon(7); //1
+	rlTal = new CANTalon(8); //2
+	frTal = new CANTalon(5); //3
+	rrTal = new CANTalon(6); //4
+	flTal->SetFeedbackDevice(CANTalon::QuadEncoder);
+	rlTal->SetFeedbackDevice(CANTalon::QuadEncoder);
+	frTal->SetFeedbackDevice(CANTalon::QuadEncoder);
+	rrTal->SetFeedbackDevice(CANTalon::QuadEncoder);
+	roboDrive = new RobotDrive(flTal, rlTal, frTal, rrTal);
+	roboDrive->SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);
+	roboDrive->SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+	roboDrive->SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
+	roboDrive->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+	gyr = new GyroSensor;
+}
+
+DriveSystem::~DriveSystem(){
+	delete flTal;
+	delete rlTal;
+	delete frTal;
+	delete rrTal;
+	delete roboDrive;
+}
+
+void DriveSystem::DriveArcade(float straight, float rotate){
+	roboDrive->ArcadeDrive(straight, rotate, false);
+}
+
+void DriveSystem::DriveTank(float left, float right){
+	roboDrive->TankDrive(left, right, false);
+}
+
+void DriveSystem::RotatetoAngle(double angle, double speed){
+	if(gyr->GyroAngleRead() < angle){
+		while(gyr->GyroAngleRead() < angle){
+			roboDrive->ArcadeDrive(0.0, PMotorPower(gyr->GyroAngleRead(), angle, turnAngleP, speed, -speed), false);
+		}
+		roboDrive->ArcadeDrive(0.0, 0.0, false);
+	}
+	else if(gyr->GyroAngleRead() > angle){
+		while(gyr->GyroAngleRead() < angle){
+			roboDrive->ArcadeDrive(0.0, PMotorPower(gyr->GyroAngleRead(), angle, turnAngleP, speed, -speed), false);
+		}
+		roboDrive->ArcadeDrive(0.0, 0.0, false);
+	}
+	else{
+		roboDrive->ArcadeDrive(0.0, 0.0, false);
+	}
+}
+
+void DriveSystem::TimeDrive(float straight, float rotate, int seconds){
+	for(int i = 0; i <= (seconds/0.03); i++){
+		roboDrive->ArcadeDrive(straight, rotate, false);
+		Wait(0.03);
+	}
+	roboDrive->ArcadeDrive(0.0, 0.0, false);
+}
+
---
+++ src/systems/DriveSystem.h
@@ -0,0 +1,44 @@
+/*
+ * DriveSystem.h
+ *
+ *  Created on: Feb 7, 2016
+ *      Author: 1750800404
+ *
+ *      Driving system for the robot
+ */
+
+#ifndef DRIVESYSTEM_H
+#define DRIVESYSTEM_H
+
+#include "CANTalon.h"
+#include "RobotDrive.h"
+#include "Timer.h"
+#include "GyroSensor.h"
+#include "util/PIDStuff.h"
+#include "util/ConstantVals.h"
+
+class DriveSystem{
+
+public:
+	DriveSystem();
+	~DriveSystem();
+
+	CANTalon *flTal;
+	CANTalon *rlTal;
+	CANTalon *frTal;
+	CANTalon *rrTal;
+	RobotDrive *roboDrive;
+	GyroSensor *gyr;
+
+	void DriveArcade(float straight, float rotate);
+	void DriveTank(float left, float right);
+	void RotatetoAngle(double angle, double speed);
+	void TimeDrive(float straight, float rotate, int seconds);
+
+private:
+
+protected:
+
+};
+
+#endif /* SRC_DRIVESYSTEM_H_ */
